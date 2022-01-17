257ef634602273e5bbe6285d3d51a46a90960d6a
Sergei S <div@altertech.com>
2022-01-17 02:35:30 +0100

    test.sh

--- test.sh
+++ test.sh
@@ -9,9 +9,6 @@ case ${CMD} in
     cargo run --release --features server,rpc --bin elbusd -- -B /tmp/elbus.sock \
       -B 0.0.0.0:9924 -B fifo:/tmp/elbus.fifo $*
     ;;
-  #benchmark)
-    #cargo run --release --bin psrt-cli --features cli -- localhost:2873 --benchmark $*
-    #;;
   cli)
     cargo run --release --bin elbus --features cli -- /tmp/elbus.sock $*
     ;;
