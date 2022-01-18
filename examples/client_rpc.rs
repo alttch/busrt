ada9f77b022a5789d9adeda8d32609dac302e7bb
Sergei S <div@altertech.com>
2022-01-18 22:30:40 +0100

    example fixes

--- examples/client_rpc.rs
+++ examples/client_rpc.rs
@@ -1,9 +1,9 @@
+use elbus::ipc::{Client, Config};
+use elbus::rpc::{DummyHandlers, Rpc, RpcClient};
 /// Demo of client RPC with no handler, which just calls methods
 ///
 /// use client_rpc_handler example to test client/server
-use elbus::empty_payload;
-use elbus::ipc::{Client, Config};
-use elbus::rpc::{DummyHandlers, Rpc, RpcClient};
+use elbus::{empty_payload, QoS};
 use serde::Deserialize;
 use std::collections::HashMap;
 
@@ -20,21 +20,27 @@ async fn main() {
     let config = Config::new("/tmp/elbus.sock", name);
     let client = Client::connect(&config).await.unwrap();
     // create RPC with no handlers
-    let mut rpc = RpcClient::new(client, DummyHandlers {});
+    let rpc = RpcClient::new(client, DummyHandlers {});
     // call the method with no confirm
-    rpc.call0(target, "test", empty_payload!()).await.unwrap();
-    let mut payload = HashMap::new();
+    rpc.call0(target, "test", empty_payload!(), QoS::Processed)
+        .await
+        .unwrap();
+    let mut payload: HashMap<&str, u32> = HashMap::new();
     payload.insert("value", 10);
     // call a method with confirm to make sure the value is added
     rpc.call(
         target,
         "add",
         rmp_serde::to_vec_named(&payload).unwrap().into(),
+        QoS::Processed,
     )
     .await
     .unwrap();
     // call the method to read the sum
-    let result = rpc.call(target, "get", empty_payload!()).await.unwrap();
+    let result = rpc
+        .call(target, "get", empty_payload!(), QoS::Processed)
+        .await
+        .unwrap();
     let amount: Amount = rmp_serde::from_read_ref(result.payload()).unwrap();
     println!("{}", amount.value);
 }
