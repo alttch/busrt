98dde5a10bb072c42b7cb25f0dcb33b58c87bf20
Serhij S <div@altertech.com>
2023-03-09 21:53:24 +0100

    comments and payload helper

--- src/cursors.rs
+++ src/cursors.rs
@@ -1,5 +1,6 @@
 use crate::rpc::RpcError;
 use async_trait::async_trait;
+use serde::{Deserialize, Serialize};
 use std::collections::BTreeMap;
 use std::sync::atomic;
 use std::sync::Arc;
@@ -9,10 +10,47 @@ use tokio::sync::RwLock;
 use tokio::task::JoinHandle;
 use uuid::Uuid;
 
+/// A helper cursor payload structure, which implements serialize/deserialize with serde
+/// Can be replaced with a custom one
+#[derive(Serialize, Deserialize, Debug, Copy, Clone)]
+pub struct Payload {
+    u: uuid::Uuid,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    c: Option<usize>,
+}
+
+impl From<Uuid> for Payload {
+    #[inline]
+    fn from(u: Uuid) -> Self {
+        Self { u, c: None }
+    }
+}
+
+impl Payload {
+    #[inline]
+    pub fn uuid(&self) -> &Uuid {
+        &self.u
+    }
+    #[inline]
+    pub fn bulk_count(&self) -> usize {
+        self.c.unwrap_or(1)
+    }
+    #[inline]
+    pub fn set_bulk_count(&mut self, c: usize) {
+        self.c = Some(c);
+    }
+    #[inline]
+    pub fn clear_bulk_count(&mut self) {
+        self.c = None;
+    }
+}
+
+/// A helper map to handle multiple cursors
 #[derive(Default)]
 pub struct Map(pub Arc<RwLock<BTreeMap<uuid::Uuid, Box<dyn Cursor + Send + Sync>>>>);
 
 impl Map {
+    /// Add a new cursor to the map and return its UUID
     pub async fn add<C>(&self, c: C) -> Uuid
     where
         C: Cursor + Send + Sync + 'static,
@@ -21,6 +59,13 @@ impl Map {
         self.0.write().await.insert(u, Box::new(c));
         u
     }
+    /// Remove a cursor from the map
+    ///
+    /// (usually should not be called, unless there is no cleaner worker spawned)
+    pub async fn remove<C>(&self, u: &Uuid) {
+        self.0.write().await.remove(u);
+    }
+    /// Call "next" method of the cursor, specified by UUID
     pub async fn next(&self, cursor_id: &Uuid) -> Result<Option<Vec<u8>>, RpcError> {
         if let Some(cursor) = self.0.read().await.get(cursor_id) {
             Ok(cursor.next().await?)
@@ -28,6 +73,7 @@ impl Map {
             Err(RpcError::not_found(None))
         }
     }
+    /// Call "next_bulk" method of the cursor, specified by UUID
     pub async fn next_bulk(
         &self,
         cursor_id: &Uuid,
@@ -39,6 +85,8 @@ impl Map {
             Err(RpcError::not_found(None))
         }
     }
+    /// Spawns a background cleaner worker, which automatically removes finished and
+    /// expired cursors from the map
     pub fn spawn_cleaner(&self, interval: Duration) -> JoinHandle<()> {
         let cursors = self.0.clone();
         let mut int = tokio::time::interval(interval);
@@ -51,6 +99,7 @@ impl Map {
     }
 }
 
+/// The cursor trait
 #[async_trait]
 pub trait Cursor {
     async fn next(&self) -> Result<Option<Vec<u8>>, RpcError>;
@@ -58,6 +107,7 @@ pub trait Cursor {
     fn meta(&self) -> &Meta;
 }
 
+/// The cursor meta object, used by cursors::Map to manage finished/expired cursors
 pub struct Meta {
     finished: atomic::AtomicBool,
     expires: Instant,
