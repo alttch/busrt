1d8e04780fa0dab654cbbfc5d672d049a69a46dd
Serhij S <div@altertech.com>
2023-04-25 01:16:45 +0200

    handler ids in topic broker

--- src/tools/pubsub.rs
+++ src/tools/pubsub.rs
@@ -8,6 +8,7 @@ pub type PublicationReceiver = async_channel::Receiver<Publication>;
 pub struct Publication {
     subtopic_pos: usize,
     frame: Frame,
+    handler_id: usize,
 }
 
 impl Publication {
@@ -49,6 +50,10 @@ impl Publication {
     pub fn is_realtime(&self) -> bool {
         self.frame.is_realtime()
     }
+    #[inline]
+    pub fn handler_id(&self) -> usize {
+        self.handler_id
+    }
 }
 
 /// Topic publications broker
@@ -58,8 +63,8 @@ impl Publication {
 /// Processes topics and sends frames to handler channels
 #[derive(Default)]
 pub struct TopicBroker {
-    prefixes: BTreeMap<String, PublicationSender>,
-    topics: BTreeMap<String, PublicationSender>,
+    prefixes: BTreeMap<String, (PublicationSender, usize)>,
+    topics: BTreeMap<String, (PublicationSender, usize)>,
 }
 
 impl TopicBroker {
@@ -78,11 +83,42 @@ impl TopicBroker {
         self.register_topic_tx(topic, tx.clone())?;
         Ok((tx, rx))
     }
+    /// Process a topic (returns tx, rx channel for a handler)
+    ///
+    /// handler id - a custom handler id (to use in multi-handlers)
+    #[inline]
+    pub fn register_topic_with_handler_id(
+        &mut self,
+        topic: &str,
+        handler_id: usize,
+        channel_size: usize,
+    ) -> Result<(PublicationSender, PublicationReceiver), Error> {
+        let (tx, rx) = async_channel::bounded(channel_size);
+        self.register_topic_tx_with_handler_id(topic, handler_id, tx.clone())?;
+        Ok((tx, rx))
+    }
     /// Process a topic with the pre-defined channel
     #[inline]
     pub fn register_topic_tx(&mut self, topic: &str, tx: PublicationSender) -> Result<(), Error> {
         if let Entry::Vacant(o) = self.topics.entry(topic.to_owned()) {
-            o.insert(tx);
+            o.insert((tx, 0));
+            Ok(())
+        } else {
+            Err(Error::busy("topic already registered"))
+        }
+    }
+    /// Process a topic with the pre-defined channel
+    ///
+    /// handler id - a custom handler id (to use in multi-handlers)
+    #[inline]
+    pub fn register_topic_tx_with_handler_id(
+        &mut self,
+        topic: &str,
+        handler_id: usize,
+        tx: PublicationSender,
+    ) -> Result<(), Error> {
+        if let Entry::Vacant(o) = self.topics.entry(topic.to_owned()) {
+            o.insert((tx, handler_id));
             Ok(())
         } else {
             Err(Error::busy("topic already registered"))
@@ -99,11 +135,40 @@ impl TopicBroker {
         self.register_prefix_tx(prefix, tx.clone())?;
         Ok((tx, rx))
     }
+    /// Process subtopic by prefix (returns tx, rx channel for a handler)
+    ///
+    /// handler id - a custom handler id (to use in multi-handlers)
+    #[inline]
+    pub fn register_prefix_with_handler_id(
+        &mut self,
+        prefix: &str,
+        handler_id: usize,
+        channel_size: usize,
+    ) -> Result<(PublicationSender, PublicationReceiver), Error> {
+        let (tx, rx) = async_channel::bounded(channel_size);
+        self.register_prefix_tx_with_handler_id(prefix, handler_id, tx.clone())?;
+        Ok((tx, rx))
+    }
     /// Process subtopic by prefix with the pre-defined channel
     #[inline]
     pub fn register_prefix_tx(&mut self, prefix: &str, tx: PublicationSender) -> Result<(), Error> {
         if let Entry::Vacant(o) = self.prefixes.entry(prefix.to_owned()) {
-            o.insert(tx);
+            o.insert((tx, 0));
+            Ok(())
+        } else {
+            Err(Error::busy("topic prefix already registered"))
+        }
+    }
+    /// Process subtopic by prefix with the pre-defined channel
+    #[inline]
+    pub fn register_prefix_tx_with_handler_id(
+        &mut self,
+        prefix: &str,
+        handler_id: usize,
+        tx: PublicationSender,
+    ) -> Result<(), Error> {
+        if let Entry::Vacant(o) = self.prefixes.entry(prefix.to_owned()) {
+            o.insert((tx, handler_id));
             Ok(())
         } else {
             Err(Error::busy("topic prefix already registered"))
@@ -113,19 +178,21 @@ impl TopicBroker {
     #[inline]
     pub async fn process(&self, frame: Frame) -> Result<Option<Frame>, Error> {
         if let Some(topic) = frame.topic() {
-            if let Some(tx) = self.topics.get(topic) {
+            if let Some((tx, handler_id)) = self.topics.get(topic) {
                 tx.send(Publication {
                     subtopic_pos: 0,
                     frame,
+                    handler_id: *handler_id,
                 })
                 .await?;
                 return Ok(None);
             }
-            for (pfx, tx) in &self.prefixes {
+            for (pfx, (tx, handler_id)) in &self.prefixes {
                 if topic.starts_with(pfx) {
                     tx.send(Publication {
                         subtopic_pos: pfx.len(),
                         frame,
+                        handler_id: *handler_id,
                     })
                     .await?;
                     return Ok(None);
